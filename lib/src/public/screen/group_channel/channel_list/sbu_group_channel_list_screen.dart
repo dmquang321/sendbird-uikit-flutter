758e8d3f4d6cde95839e1be1a93dd484aa787a44
quang.dominh <138613767+dmquang321@users.noreply.github.com>
2025-03-24 11:48:47 +0700

    Revert "add ValueNotifier to query list chat"
    This reverts commit 1721c6a53c941beaed6d25520935028b5441ad61.

--- lib/src/public/screen/group_channel/channel_list/sbu_group_channel_list_screen.dart
+++ lib/src/public/screen/group_channel/channel_list/sbu_group_channel_list_screen.dart
@@ -17,10 +17,6 @@ import 'package:sendbird_uikit/src/internal/provider/sbu_group_channel_collectio
 import 'package:sendbird_uikit/src/internal/resource/sbu_text_styles.dart';
 import 'package:sendbird_uikit/src/internal/utils/sbu_reaction_manager.dart';
 
-class QueryController extends ValueNotifier<GroupChannelListQuery?> {
-  QueryController({GroupChannelListQuery? query}) : super(query);
-}
-
 /// SBUGroupChannelListScreen
 class SBUGroupChannelListScreen extends SBUStatefulComponent {
   static const double defaultScrollExtentToTriggerPreloading = 2000; // Check
@@ -33,7 +29,6 @@ class SBUGroupChannelListScreen extends SBUStatefulComponent {
   final void Function(GroupChannel)? onListItemClicked;
   final double scrollExtentToTriggerPreloading;
   final double cacheExtent;
-  final QueryController? queryController;
 
   final Widget Function(
     BuildContext context,
@@ -85,7 +80,6 @@ class SBUGroupChannelListScreen extends SBUStatefulComponent {
     this.customLoadingBody,
     this.customEmptyBody,
     this.customErrorScreen,
-    this.queryController,
     super.key,
   });
 
@@ -96,7 +90,6 @@ class SBUGroupChannelListScreen extends SBUStatefulComponent {
 class SBUGroupChannelListScreenState extends State<SBUGroupChannelListScreen>
     with AutomaticKeepAliveClientMixin {
   final scrollController = ScrollController();
-  late QueryController queryController;
 
   late int collectionNo;
   bool isLoading = true;
@@ -109,9 +102,6 @@ class SBUGroupChannelListScreenState extends State<SBUGroupChannelListScreen>
   void initState() {
     super.initState();
     FToast().init(context); // Check
-    queryController =
-        widget.queryController ?? QueryController(query: widget.query);
-    queryController.addListener(_onChangeValue);
     _init();
   }
 
@@ -124,14 +114,6 @@ class SBUGroupChannelListScreenState extends State<SBUGroupChannelListScreen>
     }
 
     _loadMore();
-  }
-
-  void _onChangeValue() {
-    if (mounted) {
-      setState(() {
-        _init();
-      });
-    }
   }
 
   Future<void> _loadMore() async {
@@ -188,26 +170,10 @@ class SBUGroupChannelListScreenState extends State<SBUGroupChannelListScreen>
     }
   }
 
-   @override
-  void didUpdateWidget(covariant SBUGroupChannelListScreen oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    if (widget.queryController != oldWidget.queryController) {
-      queryController.removeListener(_onChangeValue);
-      queryController = widget.queryController ??
-          QueryController(query: widget.query);
-      queryController.addListener(_onChangeValue);
-    }
-  }
-
   @override
   void dispose() {
     SBUGroupChannelCollectionProvider().remove(collectionNo);
     scrollController.dispose();
-    queryController.removeListener(_onChangeValue);
-    if (widget.queryController == null) {
-      queryController.dispose();
-    }
     super.dispose();
   }
 
